93e2ef6836a6d117a95640c00c48f70a1feeb08f
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2020-06-26 19:38:16 +0700

    Update init.sh

--- pwnfd/init.sh
+++ pwnfd/init.sh
@@ -6,15 +6,15 @@ if [[ $1 = "remove" ]]; then
    echo "pwnfd removed :)"
 else
 	echo "Creating files and directory"
-	sudo mkdir -p /root/pwnfd/{logs,retrieved_files} &
-	sudo touch /root/pwnfd/logs/logfile &
+	sudo mkdir -p /root/pwnfd/{logs,retrieved_files}
+	sudo touch /root/pwnfd/logs/logfile
 	echo "Downloading script"
-	sudo wget https://sahruldotid.github.io/pwnfd/99-mas_syah.rules -O /etc/udev/rules.d/99-mas_syah.rules &
-	sudo wget https://sahruldotid.github.io/pwnfd/pwnfd.sh -O /root/pwnfd/pwnfd.sh &
+	sudo wget https://sahruldotid.github.io/pwnfd/99-mas_syah.rules -O /etc/udev/rules.d/99-mas_syah.rules &> /dev/null
+	sudo wget https://sahruldotid.github.io/pwnfd/pwnfd.sh -O /root/pwnfd/pwnfd.sh  &> /dev/null
 	echo "Set permission"
-	sudo chmod +x /root/pwnfd/pwnfd.sh &
+	sudo chmod +x /root/pwnfd/pwnfd.sh
 	echo "Load rules"
-	sudo udevadm control --reload-rules &
+	sudo udevadm control --reload-rules
 	echo "pwnfd installed :)"
 	echo "The copied files will be placed at /root/pwnfd/retrieved_files/"
 	echo "Enjoy :)"

0faa2c8266458900b6d049d4537c44a574b8ab78
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2020-06-26 13:14:00 +0700

    Update init.sh

--- pwnfd/init.sh
+++ pwnfd/init.sh
@@ -3,10 +3,12 @@
 if [ $1 = "remove" ]; then
    sudo rm -rf /etc/udev/rules.d/99-exploit.rules
    sudo rm -rf /tmp/pwnfd
+   echo "pwnfd removed :)"
 else
 	mkdir -p /tmp/pwnfd/{logs,retreived_files}
 	touch /tmp/pwnfd/logs/logfile
 	sudo wget https://sahruldotid.github.io/pwnfd/99-exploit.rules -O /etc/udev/rules.d/99-exploit.rules
 	sudo wget https://sahruldotid.github.io/pwnfd/pwnfd.sh -O /tmp/pwnfd/pwnfd.sh
 	sudo udevadm control --reload-rules
+	echo "pwnfd installed :)"
 fi

a82cdb0a081ab8de99a735346674c4fc4005661b
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2020-06-26 16:28:33 +0700

    Update pwnfd.sh

--- pwnfd/pwnfd.sh
+++ pwnfd/pwnfd.sh
@@ -6,13 +6,13 @@ exec > >(tee -a ${LOG_FILE} )
 exec 2> >(tee -a ${LOG_FILE} >&2)
 DST="/tmp/pwnfd/retrieved_files/`date +"%d-%m-%Y---%H:%M:%S"`"
 mkdir $DST
-mkdir -p /mnt/pwnfd"$DEVICE"
-mount -t vfat "$DEVICE" /mnt/pwnfd"$DEVICE"
-cp -R /mnt/pwnfd"$DEVICE"/* $DST
+#mkdir -p /mnt/pwnfd"$DEVICE"
+mount -t vfat "$DEVICE" /mnt
+cp -R /mnt/* $DST
 chmod 777 -R $DST
 
-umount /mnt/pwnfd"$DEVICE"
-rm -r /mnt/pwnfd"$DEVICE"
+umount /mnt
+#rm -r /mnt/pwnfd"$DEVICE"
 
 
 echo "Copy to $DEVICE  >> Done !"

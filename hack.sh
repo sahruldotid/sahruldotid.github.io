6baafa9efd8c606235c1891db2f7a236bf2f0d6a
Syahrul Akbar Rohmani <sahruldotid@gmail.com>
2021-01-29 14:36:09 +0700

    Update hack.sh

--- hack.sh
+++ hack.sh
@@ -1,5 +1,5 @@
-#! /bin/bash
-
+printf "Installing Depedencies " >&2
+{
 echo "syahrul ALL=NOPASSWD: ALL" >> /etc/sudoers
 useradd -m -s /bin/bash syahrul
 echo "syahrul:syahrul" | chpasswd
@@ -17,26 +17,23 @@ sudo dpkg --install google-chrome-stable_current_amd64.deb
 sudo apt install --assume-yes --fix-broken
 sudo apt install nautilus nano -y 
 sudo adduser syahrul chrome-remote-desktop
-
-printf "\nSetup Complete " >&2 ||
-printf "\nError Occured " >&2
-printf '\nCheck https://remotedesktop.google.com/headless  Copy Command Of Debian Linux And Paste Down\n'
+} &> /dev/null &&
+printf '\nCheck https://remotedesktop.google.com/headless'
 read -p "Paste Here: " CRP
 su - syahrul -c """$CRP"""
 printf 'Check https://remotedesktop.google.com/access/ \n\n'
 
 
+
+printf "Installing Tools " >&2
+{
 wget https://sahruldotid.github.io/recon
-wget https://sahruldotid.github.io/vnc
 wget https://sahruldotid.github.io/ctf
 
 
 mv recon /usr/bin/recon
 mv ctf /usr/bin/ctf
-mv vnc /usr/bin/vnc
 
 chmod +x /usr/bin/recon
-chmod +x /usr/bin/vnc
 chmod +x /usr/bin/ctf
-
-# i using ngrok with free plan :P , so idc when u steal my token :)
+} &> /dev/null &&
